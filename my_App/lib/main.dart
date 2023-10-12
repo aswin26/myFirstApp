f32b5b42a1e01d83dcc466d716af761143ee6d69
aswinkarthik <aswinkarthik26@gmail.com>
2023-10-12 21:28:49 +0530

    Scaffold & Styles

--- my_App/lib/main.dart
+++ my_App/lib/main.dart
@@ -1,5 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Text('Ugly Widget'),
-));
+void main() => runApp(
+  MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+        centerTitle: true,
+        backgroundColor: Colors.deepOrange[600],
+      ),
+      body: Center(
+        child: Text('Hello World!',style: TextStyle(
+          color:Colors.purpleAccent[400],
+          // backgroundColor: Colors.indigo[300],
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          fontSize: 50.0,
+          fontFamily: "YoungSerif"
+
+        )),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+        onPressed: () {
+
+        },
+        backgroundColor: Colors.orange[800],
+      ),
+    ),
+  )
+);
